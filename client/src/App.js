43786f5f3260d8b6022c77509fd47a93a85c4e02
Oskar Wrotniak <oskarwro@gmail.com>
2020-02-04 14:58:02 +0100

    Add tasks to App.js and socketIo to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,38 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+
+  state ={
+    tasks: [],
+    taskName: [],
+  };
+
+
+  componentDidMount() {
+    this.socket = io('http://localhost:8000');
+  };
+
+  clickedRemove(task){
+    const index = this.state.tasks.indexOf(task);
+    this.removeTask(index);
+  };
+
+  removeTask(id){
+    this.state.tasks.splice(id)
+    this.socket.emit('removeTask', id)
+    console.log('removeTask', id);
+  };
+
+  submitForm(event){
+    event.preventDefault();
+    this.addTask(this.state.taskName);
+  };
+
+  addTask(task) {
+    this.state.tasks.push(task);
+    this.socket.emit('addTask', task);
+  };
 
   render() {
     return (
@@ -11,11 +43,12 @@ class App extends React.Component {
         <section className="tasks-section" id="tasks-section">
           <h2>Tasks</h2>
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+            {this.state.tasks.map(task => (
+              <li key={task} className="task">{task}<button onClick={() => this.clickedRemove(task)} className="btn btn--red">Remove</button></li>
+            ))}
           </ul>
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <form onSubmit={this.submitForm} id="add-task-form">
+            <input onChange={this.state.taskName} value={this.state.taskName} className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" />
             <button className="btn" type="submit">Add</button>
           </form>
         </section>
--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
-
+const path = require('path');
+const socket = require('socket.io');
 const app = express();
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+const tasks = [];
+
+const server = app.listen( 8000, () => {
+  console.log('Server is runing on Port: 8000');
 });
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+
+  socket.on('addTask', (taskName) => {
+    tasks.push(taskName);
+    socket.broadcast.emit('addTask', taskName);
+  });
+
+  socket.on('removeTask', (taskName) => {    
+    tasks.splice(tasks.indexOf(taskName)); 
+    socket.broadcast.emit('removeTask', taskName)
+  });
 });
