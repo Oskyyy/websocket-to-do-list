70a4f7053710a835f698b7fe4f132695878bfdb6
Oskyyy <oskarwro@gmail.com>
2020-02-04 17:38:07 +0100

    finishing and fixes

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const path = require('path');
 const socket = require('socket.io');
 const app = express();
 const tasks = [];
 
 const server = app.listen( 8000, () => {
-  console.log('Server is runing on Port: 8000');
+  console.log('Server is runing on port: 8000');
 });
 
 app.use((req, res) => {
@@ -17,14 +16,16 @@ const io = socket(server);
 io.on('connection', (socket) => {
   socket.emit('updateData', tasks);
 
-
   socket.on('addTask', (taskName) => {
-    tasks.push(taskName);
-    socket.broadcast.emit('addTask', taskName);
+    if(!tasks.find(task => task.id == taskName.id)){
+      tasks.push(taskName);
+      socket.broadcast.emit('addTask', taskName);
+      };    
   });
-
-  socket.on('removeTask', (taskName) => {    
-    tasks.splice(tasks.indexOf(taskName)); 
-    socket.broadcast.emit('removeTask', taskName)
+  socket.on('removeTask', (index, task) => {
+    if(tasks.find(taskRemove => taskRemove.id == task.id)){
+      tasks.splice(index, 1);
+      socket.broadcast.emit('removeTask', index, task)
+      }    
   });
 });
